711845b71009ff1044d38bcd343262e44c9ab53c
christinepang1 <christinezpang@gmail.com>
2019-11-17 21:16:27 +0000

    header file first commit

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+
+
+#endif /* HEADER_H */
