a12777181e2fb256b7dd4ae10335829c754fda02
christinepang1 <christinezpang@gmail.com>
2019-11-19 00:23:15 +0000

    Able to run multiple times

--- main.c
+++ main.c
@@ -10,46 +10,55 @@ int main(void)
 	size_t command_size = 0;
 	size_t commandnum, i = 0;
 	char **array;
-	char *commandcopy, *token;
+	char *commandcopy = NULL, *token;
 	char delim[] = " ";
 
-	write(STDOUT_FILENO,"$",1);
-
-	getline(&command, &command_size, stdin);
-
-	for(i = 0; command[i]; i++)
+	while(1)
 	{
-		if (command[i] == '\n')
-			command[i] = '\0';
+		i = 0;
+		commandnum = 0;
+		write(STDOUT_FILENO,"$ ",2);
+
+		getline(&command, &command_size, stdin);
+
+		for(i = 0; command[i]; i++)
+		{
+			if (command[i] == '\n')
+				command[i] = '\0';
+		}
+
+		commandcopy = _strdup(command);
+
+		token = strtok(commandcopy, delim);
+		while(token)
+		{
+			write(STDOUT_FILENO, token, _strlen(token));
+			token = strtok(NULL, delim);
+			commandnum++;
+		}
+
+		array = malloc(sizeof(char *) * (commandnum + 1));
+
+		token = strtok(commandcopy, delim);
+		i = 0;
+		while (token != NULL)
+		{
+			array[i] = token;
+			token = strtok(NULL, delim);
+			i++;
+		}
+		array[i] = NULL;
+
+		if (fork() == 0)
+			execve(array[0], array, NULL);
+		else
+			wait(NULL);
+		free(command);
+		free(commandcopy);
+		free(array);
+
+		command = NULL;
+		commandcopy = NULL;
 	}
-
-	commandcopy = _strdup(command);
-
-	token = strtok(commandcopy, delim);
-	while(token)
-	{
-		write(STDOUT_FILENO, token, _strlen(token));
-		token = strtok(NULL, delim);
-		commandnum++;
-	}
-
-	array = malloc(sizeof(char *) * (commandnum + 1));
-	if (array == NULL)
-		return(-1);
-
-	token = strtok(commandcopy, delim);
-	i = 0;
-	while (token != NULL)
-	{
-		array[i] = token;
-		token = strtok(NULL, delim);
-		i++;
-	}
-	array[i] = NULL;
-
-	if (fork() == 0)
-		execve(array[0], array, NULL);
-	else
-		wait(NULL);
 	return(0);
 }
